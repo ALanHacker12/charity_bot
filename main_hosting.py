9f64b7e7119a803af23a690fd5d48dba08caeaab
ALanHacker12 <125786855+ALanHacker12@users.noreply.github.com>
2026-02-17 23:39:10 +0500

    Add files via upload
    Загружаю бота

---
+++ main_hosting.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+import config
+from handlers import router
+
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    # Создаем объекты бота и диспетчера
+    bot = Bot(token=config.TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+    
+    # Подключаем router
+    dp.include_router(router)
+    
+    # Запускаем бота (на хостинге используем polling)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
