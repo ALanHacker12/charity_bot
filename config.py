c438f771c1b685ab3e4d258f0716680637cfe0ee
ALanHacker12 <125786855+ALanHacker12@users.noreply.github.com>
2026-02-18 09:16:29 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
-TOKEN = "8365109516:AAFrZj0fTHCriYKRJP9FJKgj7FGknXR3XK8"
+import os
 
-ADMIN_CHAT_ID = "366700120"
+TOKEN = os.getenv('TOKEN', '8365109516:AAFrZj0fTHCriYKRJP9FJKgj7FGknXR3XK8')
+ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID', '366700120')
+
+if not TOKEN:
+    raise ValueError("Токен не найден! Добавьте переменную окружения TOKEN")
