85cb1b8ab3d2c9367d021d33e8fdd85cbb518bd5
ALanHacker12 <125786855+ALanHacker12@users.noreply.github.com>
2026-02-18 09:17:20 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,16 +1,17 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-# Функция, которая создает и возвращает главное меню с 4 кнопками
 def get_main_keyboard():
-    # Создаем клавиатуру. resize_keyboard=True делает кнопки маленькими и удобными.
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    # Создаем сами кнопки
+    # Создаем кнопки
     buttons = [
-        KeyboardButton(text="📦 Отправить продукцию"),
-        KeyboardButton(text="🍎 Купить питание"),
-        KeyboardButton(text="🧵 Своими руками"),
-        KeyboardButton(text="💰 Помочь деньгами")
+        [KeyboardButton(text="📦 Отправить продукцию")],
+        [KeyboardButton(text="🍎 Купить питание")],
+        [KeyboardButton(text="🧵 Своими руками")],
+        [KeyboardButton(text="💰 Помочь деньгами")]
     ]
-    # Добавляем кнопки на клавиатуру
-    keyboard.add(*buttons) # Звездочка здесь означает "распаковать список"
+    
+    # Создаем клавиатуру
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=buttons,
+        resize_keyboard=True
+    )
     return keyboard
